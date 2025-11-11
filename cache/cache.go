45aeb0909ff70dc38a850cf7430e92b44bb2a8aa
K0peika <priv.chel0vek@gmail.com>
2025-11-11 22:06:55 +0200

    correct library

--- cache/cache.go
+++ cache/cache.go
@@ -1,25 +1,25 @@
 package cache
 
-import (
-	"log"
-)
-
-var cache map[string]any
-
-func Set(key string, value any) {
-	cache = map[string]any{
-		key: value,
-	}
+type Cache struct {
+	cache map[string]any
 }
 
-func Get(key string) any {
-	if _, ok := cache[key]; ok {
-		return cache[key]
+func New() *Cache {
+	return &Cache{
+		cache: make(map[string]any),
+	}
+}
+func (c *Cache) Set(key string, value any) {
+	c.cache[key] = value
+}
+
+func (c *Cache) Get(key string) any {
+	if _, ok := c.cache[key]; ok {
+		return c.cache[key]
 	}
 	return "key NOT found"
 }
 
-func Delete(key string) {
-	cache[key] = nil
-	log.Printf("deletion of %s - successful", key)
+func (c *Cache) Delete(key string) {
+	delete(c.cache, key)
 }
